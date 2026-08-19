df279cc3b9c205a418e901cc0d600037070ea966
bbdd <179790579+bbdd2729@users.noreply.github.com>
2026-08-20 05:16:09 +0800

    :tada:

---
+++ LightUpTest/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace LightUpTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ LightUpUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LightUpUI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
